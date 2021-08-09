dd0a9cec5d18453bafcc0f1d7d7dc51359a04c1c
ananthu <[ananthuvngmkd@gmail.com]>
2021-08-09 17:41:24 +0530

    new

---
+++ lib/dash.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              width: width,
+              height: height,
+            ),
+            Positioned(
+              bottom: height * .015,
+              right: width * -.08,
+              child: Container(
+                width: width * .35,
+                height: height * .75,
+                child: Image(
+                  image: AssetImage('images/sancharibg.png'),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 15,
+              left: 10,
+              child: IconButton(
+                icon: Image.asset('images/drawer.png'),
+                iconSize: 40,
+                onPressed: () {},
+              ),
+            ),
+            Positioned(
+              top: 0,
+              right: 10,
+              child: IconButton(
+                icon: Image.asset('images/chat.png'),
+                iconSize: 80,
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sanchari_ui/dash.dart';
 
 void main() {
   runApp(MyApp());
@@ -6,37 +7,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       home: Home(),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-              top: height * .1,
-              right: width * -.05,
-              child: Container(
-                  width: width * .35,
-                  height: height * .85,
-                  child: Image(image: AssetImage('images/sancharibg.png'))))
-        ],
-      ),
     );
   }
 }
